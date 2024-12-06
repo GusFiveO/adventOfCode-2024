55834bd80bf939155be447cad0385a33400fcbc5
Augustin LORAIN <GusFiveO@users.noreply.github.com>
2024-12-06 13:47:24 +0100

    day06 P1 done

--- day06/count_guard_position.py
+++ day06/count_guard_position.py
@@ -12,32 +12,33 @@ class State(Enum):
 
 
 class Guard:
-    def __init__(self, direction_symbol, position, map):
-        self.direction = directions[direction_symbol]
-        self.position = position
+    def __init__(self, map):
+        (self.direction, self.position) = self.find_initial_position(map)
+        self.visited_postions = 0
         self.map = map
         self.state = State.FREE
 
     def __str__(self):
         return f"direction: {self.direction}, position: {self.position}"
 
-    def find_initial_position(map):
+    def find_initial_position(self, map):
         for row_idx, row in enumerate(map):
             for col_idx, elem in enumerate(row):
-                if elem in directions.keys:
-                    return (directions[elem], (row_idx, col_idx))
+                if elem in directions.keys():
+                    return elem, (row_idx, col_idx)
+        return None, None
 
     def turn_right(self):
-        direction_symbol = rotation[self.direction]
-        self.direction = directions[direction_symbol]
+        self.direction = rotation[self.direction]
+        self.state = State.FREE
 
     def is_out(self, position):
         map_width = len(self.map[0])
         map_height = len(self.map)
         if (
-            position[0] > map_height
+            position[0] >= map_height
             or position[0] < 0
-            or position[1] > map_width
+            or position[1] >= map_width
             or position[1] < 0
         ):
             return True
@@ -49,30 +50,46 @@ class Guard:
         return False
 
     def mark_visited_position(self, position):
-        print(self.map[position[0]][position[1]])
+        if self.map[position[0]][position[1]] != "X":
+            self.visited_postions += 1
         row = list(self.map[position[0]])
         row[position[1]] = "X"
         self.map[position[0]] = "".join(row)
 
     def move_forward(self):
-        (y_step, x_step) = self.direction
+        (y_step, x_step) = directions[self.direction]
         initial_position = self.position
         new_position = (self.position[0] + y_step, self.position[1] + x_step)
         if self.is_out(new_position):
             self.mark_visited_position(initial_position)
             self.state = State.OUT
+            return
         elif self.is_blocked(new_position):
             self.state = State.BLOCKED
+            return
         else:
             self.mark_visited_position(initial_position)
             self.state = State.FREE
+        self.position = new_position
+
+    def patrol(self):
+        while self.state is not State.OUT:
+            while self.state is State.FREE:
+                self.move_forward()
+            if self.state is State.BLOCKED:
+                self.turn_right()
+        return self.visited_postions
+
+    def print_map(self):
+        for row in self.map:
+            print(row)
 
 
 guard_map_path = sys.argv[1]
 with open(guard_map_path) as file:
     lines = file.read().splitlines()
     guard_map = lines
-guard = Guard(">", (0, 0), guard_map)
-guard.move_forward()
-print(guard)
-print(guard_map)
+guard = Guard(guard_map)
+visited_position = guard.patrol()
+guard.print_map()
+print(f"The guard visited {visited_position} positions !")
