91a5fb0e3813ee5a2f50303c28a30c435f964ead
Augustin LORAIN <GusFiveO@users.noreply.github.com>
2024-12-06 18:28:16 +0100

    ugly & slow but finally work

--- day06/count_loop_obstructions_refacto.py
+++ day06/count_loop_obstructions_refacto.py
@@ -57,24 +57,24 @@ class Guard:
             return
         elif self.is_blocked(new_position):
             self.turn_right()
-            # self.move_forward()
             return
         # else:
         #     self.state = State.FREE
         self.position = new_position
 
-    def add_obstacle_forward(self):
+    def add_obstacle_forward(self, position):
         map_width = len(self.map[0])
         map_height = len(self.map)
-        position = (
-            self.position[0] + directions[self.direction][0],
-            self.position[1] + directions[self.direction][1],
-        )
+        # position = (
+        #     self.position[0] + directions[self.direction][0],
+        #     self.position[1] + directions[self.direction][1],
+        # )
         if (
             position[0] >= map_height
             or position[0] < 0
             or position[1] >= map_width
             or position[1] < 0
+            or self.map[position[0]][position[1]] != "."
         ):
             return False
         row = list(self.map[position[0]])
@@ -85,12 +85,11 @@ class Guard:
     def find_obstacles(self):
         obstacles = set()
         i = 0
-        while self.state is not State.OUT and i < 100000:
+        while self.state is not State.OUT:
             tmp_guard = copy.deepcopy(self)
-            if tmp_guard.add_obstacle_forward() and is_in_a_loop(
-                tmp_guard, self.position, self.direction
-            ):
-                print("LOOP", i)
+            # if tmp_guard.add_obstacle_forward() and is_in_a_loop(
+            if is_in_a_loop(tmp_guard, self.position, self.direction):
+                # print("LOOP")
                 position = (
                     self.position[0] + directions[self.direction][0],
                     self.position[1] + directions[self.direction][1],
@@ -114,21 +113,12 @@ def check_history(history, direction, position):
     return False
 
 
-def is_in_a_loop(guard: Guard, initial_position, initial_direction):
+def is_in_a_loop(guard: Guard):
     history = {"<": [], "^": [], ">": [], "v": []}
+    # print_map(guard.map)
     while guard.state is not State.OUT:
         if check_history(history, guard.direction, guard.position):
-            # print("initial", initial_position, initial_direction)
-            # print("guard", guard.position, guard.direction)
-            # guard.move_forward()
-            # if guard.position == initial_position:
-            #     print("initial", initial_position, initial_direction)
-            #     print("guard", guard.position, guard.direction)
-            # if (
-            #     guard.position[0] == initial_position[0]
-            #     and guard.position[1] == initial_position[1]
-            #     and guard.direction == initial_direction
-            # ):
+            print("loop", guard)
             return True
         else:
             history[guard.direction].append(guard.position)
@@ -140,8 +130,31 @@ guard_map_path = sys.argv[1]
 with open(guard_map_path) as file:
     lines = file.read().splitlines()
     guard_map = lines
-guard = Guard(guard_map)
-print_map(guard.map)
-obstacles = guard.find_obstacles()
-length = len(obstacles)
-print(length)
+# guard = Guard(guard_map)
+# print_map(guard.map)
+# obstacles = guard.find_obstacles()
+# length = len(obstacles)
+# print(length)
+guard2 = Guard(guard_map.copy())
+count = 0
+for j, row in enumerate(range(len(guard_map))):
+    for i, col in enumerate(range(len(guard_map[0]))):
+        # guard2 = Guard(guard_map.copy())
+        guard2.map = guard_map.copy()
+        guard2.state = State.FREE
+        (guard2.direction, guard2.position) = guard2.find_initial_position(guard2.map)
+
+        if guard2.add_obstacle_forward((row, col)):
+            # print_map(guard2.map)
+            # if i == 60 and j == 64:
+            if is_in_a_loop(guard2) is True:
+                count += 1
+            # if i == 60 and j == 64:
+            #     print_map(guard2.map)
+            #     print("==================================")
+            #     if is_in_a_loop(guard2):
+            #         print("loop")
+            #         count += 1
+            #         continue
+            #     exit()
+print(count)
